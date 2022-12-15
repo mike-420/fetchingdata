0e780d1ed0b9a1659cdf642688f9736e816e2a0f
mike-420 <61431978+mike-420@users.noreply.github.com>
2022-12-14 19:28:22 -0800

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+</template>
+<script setup>
+  const { data : users } = await useFetch('https://jsonplaceholder.typicode.com/users')
+</script>
+
